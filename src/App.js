94a566f7c715b0fa8726a86e6c5f368d40525c50
Precious <Preshomondi@gmail.com>
2023-03-29 01:45:22 +0300

    Enable dragging only by box2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React, {useRef} from 'react';
-import './App.css';
-import {motion} from "framer-motion"
+import React, { useRef } from "react";
+import "./App.css";
+import { motion, useDragControls } from "framer-motion";
 
 function App() {
-  const parentRef = useRef()
+  const parentRef = useRef();
+  const controls = useDragControls()
   return (
     <div className="App" ref={parentRef}>
-      <motion.div className='box1'
-      drag
-      dragConstraints={parentRef}
-      dragSnapToOrigin="true"
+      <motion.div
+        className="box1"
+        drag
+        dragConstraints={parentRef}
+        dragControls={controls}
+        dragListener={false}
+       
       >
+        <div className="box2"
+        onPointerDown={(e) => controls.start(e)}
+         >
 
+        </div>
       </motion.div>
-     
     </div>
   );
 }
