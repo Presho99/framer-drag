8672938a357d1ba28a139771f5bb66dcf66dc3c8
Precious <Preshomondi@gmail.com>
2023-03-29 00:12:03 +0300

    Add a drag effect

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import {motion, MotionConfig} from "framer-motion"
 function App() {
   return (
     <div className="App">
-      <motion.div className='box1'>
+      <motion.div className='box1'
+      drag
+      >
 
       </motion.div>
      
